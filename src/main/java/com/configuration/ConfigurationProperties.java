2789f577ede29cb86a58ee3dfe4529f8f6c254fc
dharm519 <dharmendra519@outlook.com>
2022-11-22 08:30:50 +0530

    pom, config file changes

---
+++ src/main/java/com/configuration/ConfigurationProperties.java
@@ -0,0 +1,27 @@
+package com.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serializable;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"products", "promotions"})
+public class ConfigurationProperties {
+
+    @JsonProperty("products")
+    private List<ProductProperties> products;
+    @JsonProperty("promotions")
+    private List<PromotionProperties> promotions;
+
+    public List<ProductProperties> getProducts() {
+        return products;
+    }
+
+    public List<PromotionProperties> getPromotions() {
+        return promotions;
+    }
+}
+
---
+++ src/main/java/com/configuration/ProductProperties.java
@@ -0,0 +1,25 @@
+package com.configuration;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serializable;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"name", "price"})
+public class ProductProperties {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("price")
+    private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+}
